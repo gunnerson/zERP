ce393b10136c000c0a7f1f3754acd458cf3272aa
bwaargh <rawpluganchor.com>
2020-03-29 20:39:08 -0500

    Changed settings

--- erp/settings.py
+++ erp/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x$ruwio0cp5lm1bx2r@)_6v97gl=@2cjirn43-or3ibzb#@a3z'
+# SECRET_KEY = 'x$ruwio0cp5lm1bx2r@)_6v97gl=@2cjirn43-or3ibzb#@a3z'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'x$ruwio0cp5lm1bx2r@)_6v97gl=@2cjirn43-or3ibzb#@a3z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
