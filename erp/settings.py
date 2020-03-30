7ea3416fc1865a5a9c335ff6871dcb49790ec0fb
bwaargh <rawpluganchor@gmail.com>
2020-03-30 10:24:08 -0500

    Changed settings.

--- erp/settings.py
+++ erp/settings.py
@@ -154,7 +154,7 @@ if os.getcwd() == '/app':
 	# Static asset configuration
 	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 	STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-	STATIC_URL = '/static/'
+	STATIC_URL = '/home/vadim/python_projects/rahco/erp/static/'
 	STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
