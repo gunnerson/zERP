c31559eb43eb95ea74ad96ccb91f03613985ac15
bwaargh <rawpluganchor@gmail.com>
2020-04-06 09:34:38 -0500

    Switched orders to Listview

--- mtn/urls.py
+++ mtn/urls.py
@@ -1,47 +1,23 @@
-"""mtn URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import path
 
 from . import views
+from mtn.views import OrderListView
 
 app_name = "mtn"
 
 urlpatterns = [
-	# Home page
+
     path('', views.index, name='index'),
     
-    # Maintenance page
-    path('maint/', views.maint, name='maint'),
-    
-    # Show all open work orders
-    path('maint/orders/', views.orders, name='orders'),
+    path('mwo/', views.maint, name='maint'),
 
-    # Show all open work orders
-    path('maint/closed_orders/', views.closed_orders, 
-			name='closed_orders'),
-    
-    # Detail page for a single work order
-    path('maint/order/<int:id>/', views.order, 
+    path('mwo/order-list/', OrderListView.as_view(), name='order-list'),
+
+    path('mwo/order/<int:order_id>/', views.order, 
 			name='order'),
     
-    # Page for adding new work orders
-    path('maint/new_order/', views.new_order, name='new_order'),
+    path('mwo/new_order/', views.new_order, name='new_order'),
 	
-    # Page for editing a repair
-    path('maint/edit_order/<int:id>/', views.edit_order, 
+    path('mwo/edit_order/<int:order_id>/', views.edit_order, 
 		    name='edit_order'),
 ]
--- mtn/views.py
+++ mtn/views.py
@@ -3,114 +3,113 @@ from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.forms import inlineformset_factory
+from django.views.generic import ListView, DetailView, CreateView
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Order
 from .forms import OrderForm, RepairForm
 
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists() 
+
+def is_valid_queryparam(param):
+    return param != '' and param is not None
     
 def index(request):
-	"""The home page for EPR"""
-	return render(request, 'mtn/index.html')
+    """The home page for EPR"""
+    return render(request, 'mtn/index.html')
 
 @login_required
 def maint(request):
-	"""The home page for Maintenance"""
-	return render(request, 'mtn/maint.html')
-	
-@login_required
-def orders(request):
-	"""Show all open orders."""
-	orders = Order.objects.filter(owner=request.user, closed=False).order_by('-date_added')
-	context = {'orders': orders}
-	return render(request, 'mtn/orders.html', context)
+    """The home page for Maintenance"""
+    return render(request, 'mtn/maint.html')
+    
+class OrderListView(LoginRequiredMixin, ListView):
+
+    model = Order
+
+    def get_queryset(self):
+        qs = Order.objects.all().order_by('-date_added')
+        check_closed = self.request.GET.get('check_closed')
+        if is_valid_queryparam(check_closed):
+            qs = qs.filter(closed=check_closed)
+        else:
+            qs = qs.filter(closed=False)        
+        return qs
 
 @login_required
-def closed_orders(request):
-	"""Show all closed orders."""
-	orders = Order.objects.filter(owner=request.user, closed=True).order_by('-date_added')
-	context = {'orders': orders}
-	return render(request, 'mtn/orders.html', context)
+def order(request, order_id):
+    """Show a single order and all its entries."""
+    if (has_group(request.user, 'maintenance') or 
+        has_group(request.user, 'supervisor')):
+            order = get_object_or_404(Order, id=order_id)
+        
+    else:
+        raise Http404
 
-@login_required
-def order(request, id):
-	"""Show a single order and all its entries."""
-	if (has_group(request.user, 'maintenance') or 
-		has_group(request.user, 'supervisor')):
-			order = get_object_or_404(Order, id=id)
-		
-	else:
-		raise Http404
-
-	context = {'order': order,}
-	return render(request, 'mtn/order.html', context)
+    context = {'order': order,}
+    return render(request, 'mtn/order.html', context)
 
 @login_required
 def new_order(request):
-	"""Add new order"""
-	if (has_group(request.user, 'maintenance') or 
-		has_group(request.user, 'supervisor')):
-	
-		if request.method != 'POST':
-			# No data submitted; create a blank form.
-			form = OrderForm()
+    """Add new order"""
+    if (has_group(request.user, 'maintenance') or 
+        has_group(request.user, 'supervisor')):
+    
+        if request.method != 'POST':
+            # No data submitted; create a blank form.
+            form = OrderForm()
 
-		else:
-		# POST data submitted; process data.
-			form = OrderForm(data=request.POST)
-			if form.is_valid():
-				new_order = form.save(commit=False)
-				new_order.owner = request.user
-				new_order.save()
-				return HttpResponseRedirect(reverse('mtn:orders'))
-	
-	else:
-		raise Http404		
-											
-	context = {'form': form}
-	return render(request, 'mtn/new_order.html', context)
-	
+        else:
+        # POST data submitted; process data.
+            form = OrderForm(data=request.POST)
+            if form.is_valid():
+                new_order = form.save(commit=False)
+                new_order.owner = request.user
+                new_order.save()
+                return HttpResponseRedirect(reverse('mtn:order-list'))
+    
+    else:
+        raise Http404       
+                                            
+    context = {'form': form}
+    return render(request, 'mtn/new_order.html', context)
+    
 @login_required
-def edit_order(request, id):
-	"""Edit an existing repair."""
-	if has_group(request.user, 'maintenance'):
-		order = Order.objects.get(id=id)
+def edit_order(request, order_id):
+    """Edit an existing repair."""
+    if has_group(request.user, 'maintenance'):
+        order = Order.objects.get(id=order_id)
 
-		if request.method != 'POST':
-			# Initial request; pre-fill form with the current repair.
-			form = RepairForm(instance=order)
+        if request.method != 'POST':
+            # Initial request; pre-fill form with the current repair.
+            form = RepairForm(instance=order)
 
-		else:
-		# POST data submitted; process data.
-			form = RepairForm(instance=order, data=request.POST)
-			if form.is_valid():
-				form.save()
-				return HttpResponseRedirect(reverse('mtn:order',
-												args=[order.id]))
-	else:
-		raise Http404
+        else:
+        # POST data submitted; process data.
+            form = RepairForm(instance=order, data=request.POST)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(reverse('mtn:order',
+                                                args=[order.id]))
+    else:
+        raise Http404
 
-	context = {'order': order, 'form': form}
-	return render(request, 'mtn/edit_order.html', context)	
-
-@login_required
-def orders_bypress(request, press_id):
-	"""Show all orders for this press."""
-	orders = Order.objects.filter(local=press_id).order_by('-date_added')
-	context = {'orders': orders}
-	return render(request, 'mtn/orders.html', context)
+    context = {'order': order, 'form': form}
+    return render(request, 'mtn/edit_order.html', context)  
 
 # def add_part(request, order_id):
-# 	order = Order.objects.get(pk=order_id)
-# 	PartFormset = inlineformset_factory(Order, Part, fields=('partname',), extra=1)
+#   order = Order.objects.get(pk=order_id)
+#   PartFormset = inlineformset_factory(Order, Part, fields=('partname',), extra=1)
 
-# 	if request.method == 'POST':
-# 		formset = PartFormset(request.POST, instance=order)
-# 		if formset.is_valid():
-# 			formset.save
-# 			return redirect('', order_id=order.id)
+#   if request.method == 'POST':
+#       formset = PartFormset(request.POST, instance=order)
+#       if formset.is_valid():
+#           formset.save
+#           return redirect('', order_id=order.id)
 
 # formset = PartFormset(instance=order)
 
 # return render(request, 'index.html', {'formset': formset})
+
