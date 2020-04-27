90dc269de4460b0cc16f1f0e2323fc2f27a256dd
bwaargh <rawpluganchor@gmail.com>
2020-04-27 13:18:53 -0500

    Deploying to Heroku

--- erp/settings.py
+++ erp/settings.py
@@ -140,3 +140,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 # My settings
 LOGIN_URL = '/users/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Heroku
+# if os.getcwd() == '/app':
+#     import dj_database_url
+#     DATABASES = {
+#         'default': dj_database_url.config(default='postgres://localhost')
+#     }
+#     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+#     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#     # Allow all host headers.
+#     ALLOWED_HOSTS = ['*']
+#     # Static asset configuration
+#     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#     STATIC_ROOT = 'staticfiles'
+#     STATICFILES_DIRS = (
+#         os.path.join(BASE_DIR, 'static'),
+#     )
