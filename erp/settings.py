51771a5655369362428d4076ca6955e2fb1113a4
bwaargh <rawpluganchor@gmail.com>
2020-06-23 13:42:50 -0500

    fix

--- erp/settings.py
+++ erp/settings.py
@@ -170,6 +170,4 @@ if os.getcwd() == '/app' or os.getcwd()[:4] == '/tmp':
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     # STATIC_ROOT = 'staticfiles'
     STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    STATICFILES_DIRS = ('/home/vadim/python_projects/rahco/static', )
